626e18ee8c20ca47ec398e829c5e696a1391e23e
hstarorg <hm910705@163.com>
2026-05-10 23:46:42 +0800

    feat(web): add favicon

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -6,7 +6,16 @@ export function Layout() {
   return (
     <div className="app">
       <header className="header">
-        <Link to="/" className="logo">{t("header.logo")}</Link>
+        <Link to="/" className="logo">
+          <img
+            src="/favicon.svg"
+            alt=""
+            className="logo-mark"
+            width={28}
+            height={28}
+          />
+          <span>{t("header.logo")}</span>
+        </Link>
       </header>
       <main className="main">
         <Outlet />
